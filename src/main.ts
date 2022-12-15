296bca8c097fe89c1a249e28fee894668093db26
Wangnov <48670012+Wangnov@users.noreply.github.com>
2022-12-15 17:17:08 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function main() {
     .on("message", async (message: any) => {
       try {
         console.log(`📨 ${message}`);
-        console.log(message.from);
+        console.log(message.from());
         // add your own task handlers over here to expand the bot ability!
         // e.g. if a message starts with "Hello", the bot sends "World!"
         if (message.text.startsWith("Hello")) {
