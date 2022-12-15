3b6f74d9f122a9ba502e0f8122547d5fc9fd05bb
Wangnov <48670012+Wangnov@users.noreply.github.com>
2022-12-15 17:23:04 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -29,11 +29,10 @@ async function main() {
     .on("message", async (message: any) => {
       try {
         console.log(`📨 ${message}`);
-        console.log(message.from());
         // add your own task handlers over here to expand the bot ability!
         // e.g. if a message starts with "Hello", the bot sends "World!"
         if (message.text.startsWith("Hello")) {
-          await message.say("World!");
+          await message.say(message.from());
           return;
         }
         // handle message for chatGPT bot
